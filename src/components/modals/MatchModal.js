2ffca94542dcd9ecdc54bf50bccf1b05bcad52df
SurasithIT <59070181@it.kmitl.ac.th>
2020-10-06 23:37:21 +0700

    init Match Modal

---
+++ src/components/modals/MatchModal.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const MatchModal = () => {
+  return (
+    <div>
+      <div>
+        {/* Button trigger modal */}
+        {/* <button
+          type="button"
+          className="btn btn-primary"
+          data-toggle="modal"
+          data-target="#staticBackdrop"
+        >
+          Launch static backdrop modal
+        </button> */}
+        {/* Modal */}
+        <div
+          className="modal fade"
+          id="staticBackdrop"
+          data-backdrop="static"
+          tabIndex={-1}
+          role="dialog"
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="staticBackdropLabel">
+                  Modal title
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <div className="modal-body">...</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Understood
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="modal-backdrop" style={{ opacity: "30%" }}></div> */}
+    </div>
+  );
+};
+
+export default MatchModal;
--- src/components/pages/Match.js
+++ src/components/pages/Match.js
@@ -1,7 +1,79 @@
 import React from "react";
+import MatchModal from "../modals/MatchModal";
 
 const Match = () => {
-  return <div>Match Content</div>;
+  return (
+    <div>
+      <MatchModal></MatchModal>
+      <div className="content">
+        <div className="card content-inside">
+          <div className="row">
+            <div className="col-sm">Match Content</div>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <table className="table table-hover">
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col-2">Stadium</th>
+                    <th scope="col-3">Home</th>
+                    <th scope="col-1"></th>
+                    <th scope="col-1"></th>
+                    <th scope="col-1"></th>
+                    <th scope="col-3">Away</th>
+                    <th scope="col-2"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Team 1</td>
+                    <td>0</td>
+                    <td>-</td>
+                    <td>0</td>
+                    <td>Team 2</td>
+                    <td>
+                      <button className="btn btn-primary">Edit</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Team 1</td>
+                    <td>0</td>
+                    <td>-</td>
+                    <td>0</td>
+                    <td>Team 2</td>
+                    <td>
+                      <button className="btn btn-primary">Edit</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Team 1</td>
+                    <td>0</td>
+                    <td>-</td>
+                    <td>0</td>
+                    <td>Team 2</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        data-toggle="modal"
+                        data-target="#staticBackdrop"
+                      >
+                        {/* <button className="btn btn-primary" onClick={toggle}> */}
+                        Edit
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Match;
